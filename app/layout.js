3d0f3a6ce8b0b2bd7d7f64144a13a0cafdf4107c
Mie Harada <miryna1978@gmail.com>
2024-05-15 13:24:01 +0900

    Trigger new deployment

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,6 @@ import "./styles/singleBlog.css"
 import Header from "./components/header";
 import Footer from "./components/footer";
 
-
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
